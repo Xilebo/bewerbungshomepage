36f55aba00b1da4b237d3f9d7cca042797a5993c
unknown <proplan-tln27@projekt-grafik3.btz-tln.local>
2015-01-07 15:21:16 +0100

    Veränderung der Breite entfernt. Es werden nur noch divs der Klasse .expandable animiert.
    Signed-off-by: unknown <proplan-tln27@projekt-grafik3.btz-tln.local>

--- js/dropdownmultiple.js
+++ js/dropdownmultiple.js
@@ -1,27 +1,24 @@
 $(document).ready(function(){
   
-  $("div").each(function(){
+  $(".expandable").each(function(){
     $(this).data('defaultheight', $(this).height());
     $(this).data('state', false);
   });
 
-$("div").animate({
-         width: 300,
+$(".expandable").animate({
          height: 20
          }, 0 );
 
-  $("div").click(function() {
+  $(".expandable").click(function() {
      defaultheight = $(this).data('defaultheight');
      state = $(this).data('state');
      if ( state ) {
      
      $(this).animate({
-         width: 300,
          height: 20
          }, 1000 );
      } else {
      $( this ).animate({
-         width: 300,
          height: defaultheight 
      }, 1000 );
      }
