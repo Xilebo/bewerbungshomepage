ab06d6764cef6c5b52b47caf98419f83c194c342
Till <proplan-tln08@btz-tln.local>
2015-01-12 13:37:47 +0100

    add a class for open and closed expandables

--- js/dropdownmultiple.js
+++ js/dropdownmultiple.js
@@ -11,20 +11,25 @@ $(document).ready(function(){
      defaultheight = $object.data('defaultheight');
      state = $object.data('state');
      if ( state ) {
-
-     $object.animate({
-         height: 20
+		$object.animate({
+			height: 20
          }, $time );
+		 $object.data('state', false);
+		 $object.addClass('closed');
+		 $object.removeClass('open');
      } else {
-     $object.animate({
-         height: defaultheight
-     }, $time );
+		 $object.animate({
+			 height: defaultheight
+		 }, $time );
+		 $object.data('state', true);
+		 $object.addClass('open');
+		 $object.removeClass('closed');
      }
-     $object.data('state', !state);
+
   }
 
-   toggleExpansion($(".expandable"), 0);
-  $(".expandable").click(function() {
-	toggleExpansion($(this),1000);
+   toggleExpansion($(".expandable > span").parent(), 0);
+  $(".expandable > span").click(function() {
+	toggleExpansion($(this).parent(),1000);
   });
 });
