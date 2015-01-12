ad957aa6f53f01b1b4ba5d1d0bdbe40864222556
Daniel <proplan-tln27@btz-tln.local>
2015-01-12 14:25:28 +0100

    Pfeil Icons eingefügt. state in isOpen umbenannt. Einrückung verbessert.
    Signed-off-by: Daniel <proplan-tln27@btz-tln.local>

--- js/dropdownmultiple.js
+++ js/dropdownmultiple.js
@@ -1,35 +1,35 @@
 $(document).ready(function(){
 
-  $(".expandable").each(function(){
-    $(this).data('defaultheight', $(this).height());
-    $(this).data('state', true);
-  });
+	$(".expandable").each(function(){
+		$(this).data('defaultheight', $(this).height());
+		$(this).data('isOpen', true);
+	});
 
 
 
-  function toggleExpansion($object, $time) {
-     defaultheight = $object.data('defaultheight');
-     state = $object.data('state');
-     if ( state ) {
-		$object.animate({
-			height: 20
-         }, $time );
-		 $object.data('state', false);
-		 $object.addClass('closed');
-		 $object.removeClass('open');
-     } else {
-		 $object.animate({
-			 height: defaultheight
-		 }, $time );
-		 $object.data('state', true);
-		 $object.addClass('open');
-		 $object.removeClass('closed');
-     }
+	function toggleExpansion($object, $time) {
+		defaultheight = $object.data('defaultheight');
+		isOpen = $object.data('isOpen');
+		if ( isOpen ) {
+			$object.animate({
+				height: 20
+			}, $time );
+			$object.data('isOpen', false);
+			$object.addClass('closed');
+			$object.removeClass('open');
+		} else {
+			$object.animate({
+				height: defaultheight
+			}, $time );
+			$object.data('isOpen', true);
+			$object.addClass('open');
+			$object.removeClass('closed');
+		}
+	}
 
-  }
+	toggleExpansion($(".expandable > span").parent(), 0);
 
-   toggleExpansion($(".expandable > span").parent(), 0);
-  $(".expandable > span").click(function() {
-	toggleExpansion($(this).parent(),1000);
-  });
+	$(".expandable > span").click(function() {
+		toggleExpansion($(this).parent(),1000);
+	});
 });
