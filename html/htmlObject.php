fad0666b941df73f3ca3e1788111262716a085f8
Till <proplan-tln08@btz-tln.local>
2015-01-26 16:13:04 +0100

    add htmlObject.php
    htmlObject.php contains the containerclass htmlObject with some getter and
    setter functions

---
+++ html/htmlObject.php
@@ -0,0 +1,44 @@
+﻿<?php
+/**
+ * The class htmlObject represents a single object of the page.
+ */
+class htmlObject {
+	private $type = '';
+	private $content = array();
+	private $properties = array();
+
+	function __construct($type = 'span') {
+		$this->type = $type;
+	}
+
+	function getType() {
+		return $this->type;
+	}
+
+	function setType($newType) {
+		$this->type = $newType;
+	}
+
+	function addContent($newContent) {
+		$this->content[] = $newContent;
+	}
+
+	function setProperty($property, $value) {
+		$this->properties[$property] = '' . $value;
+	}
+
+	function addPropertyValue ($property, $value) {
+		$this->properties[$property] .= $value;
+	}
+
+	function getProperty($property) {
+		$result = array_key_exists ($property, $this->properties) ? $this->properties[$property] : false;
+		return $result;
+	}
+
+	function removeProperty($property) {
+		unset($this->properties[$property]);
+	}
+
+}
+?>
