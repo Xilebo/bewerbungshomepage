576e9011a1470e4e80a7308465d49bbb27c0a050
Till <proplan-tln08@btz-tln.local>
2015-01-27 08:32:07 +0100

    add toString and tabLevel to htmlObject

--- html/htmlObject.php
+++ html/htmlObject.php
@@ -4,11 +4,27 @@
  */
 class htmlObject {
 	private $type = '';
+	private $tabLevel = 0
 	private $content = array();
 	private $properties = array();
 
 	function __construct($type = 'span') {
 		$this->type = $type;
+	}
+
+	function toString() {
+		$result = '';
+
+		$result .= '<' . $type;
+		foreach ($this->properties as $property => $value) {
+			$result .= ' ' . $property . '="' . $value . '"';
+		}
+		$result .= '>' . PHP_EOL;
+		foreach ($this->content as $childelement) {
+			$result .= $childelement . PHP_EOL;
+		}
+		$result .= '</' . $type . '>'; //no EOL; EOL will be added by parent element
+		return $result;
 	}
 
 	function getType() {
@@ -19,7 +35,19 @@ class htmlObject {
 		$this->type = $newType;
 	}
 
+	function setTabLevel($newTabLevel) {
+		$this->tabLevel = $newTabLevel;
+		foreach ($this->content as $element) {
+			if (get_class($element) == 'htmlObject') {
+				$element->setTabLevel($this->tabLevel + 1);
+			}
+		}
+	}
+
 	function addContent($newContent) {
+		if (get_class($newContent) == 'htmlObject') {
+			$newContent->setTabLevel($this->tabLevel + 1);
+		}
 		$this->content[] = $newContent;
 	}
 
