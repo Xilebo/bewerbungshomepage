10acedf9b1bc97feb96ba28ef05ef209d647c81d
freshinger <gesickter@hotmail.com>
2015-04-01 12:08:15 +0200

    using slideToggle() now
    Been watching this: https://www.video2brain.com/de/tutorial/menue-um-expand-funktion-mit-effekt-erweitern-und-projekt-abschliessen 
    and improving the code using jquery's slideToggle() with css display:none.

--- js/dropdownmultiple.js
+++ js/dropdownmultiple.js
@@ -1,27 +1,9 @@
 $(document).ready(function(){
 
-	$(".expandable").each(function(){
-		$(this).data('defaultheight', $(this).height());
-		$(this).data('isOpen', false);
-	});
-
-	function toggleExpansion($object, $time) {
-		defaultheight = $object.data('defaultheight');
-		isOpen = $object.data('isOpen');
-		if ( isOpen ) {
-			$object.toggleClass( "expandable", $time, "easeOutSine" );
-			$object.data('isOpen', false);
-			$object.addClass('closed');
-			$object.removeClass('open');
-		} else {
-			$object.toggleClass( "expandable", $time, "easeOutSine" );
-			$object.data('isOpen', true);
-			$object.addClass('open');
-			$object.removeClass('closed');
-		}
-	}
-
+	$(".expandable").children("div").css("display", "none");
 	$(".expandable > span").click(function() {
-		toggleExpansion($(this).parent(),1000);
+		$(this).parent().toggleClass("open");
+		$(this).parent().children("div").slideToggle();
 	});
+	
 });
